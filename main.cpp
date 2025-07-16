448489dd9475667f6a00cbaac3ff40a63dbed0d2
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-07-15 19:15:24 -0500

    Creando interfaz al banco

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "estructuras.h"
+#include "funciones.h"
+using namespace std;
+
+int main (){
+    cout << " ======================================================================\n";
+    cout << "||                                                                    ||\n";
+    cout << "||                   Bienvenido al banco BCHP                         ||\n";
+    cout << "||                 Presione Enter para ingresar                       ||\n";
+    cout << "||                                                                    ||\n";
+    cout << " ======================================================================\n";
+    cin.get(); 
+}
