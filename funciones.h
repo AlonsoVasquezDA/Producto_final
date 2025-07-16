eddb7ce7493bb4b3b8a8c8b5955bd7880935414e
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-07-15 19:06:36 -0500

    Eliminando las variables de los prototipos

--- funciones.h
+++ funciones.h
@@ -3,9 +3,9 @@
 
 #include "estructuras.h"
 
-void leerCorreo(CORREO &c, string u, string d);
-void leerCuenta(CUENTA &c, int m, string p, double din);
-void leerDatos(DATOS &c, string n, int e, string d, long long nu, CORREO em, CUENTA cu);
-void mostrarDatos(DATOS &c);
+void leerCorreo(CORREO &, string , string );
+void leerCuenta(CUENTA &, int , string , double );
+void leerDatos(DATOS &, string , int , string , long long , CORREO , CUENTA );
+void mostrarDatos(DATOS &);
 
 #endif 
